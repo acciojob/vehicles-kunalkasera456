2ebda0e6ebefb2c31fad6b0f9228b3deb2218c08
Kunal Kasera <kunalkasera456@gmail.com>
2023-02-20 17:26:49 +0530

    chage public to private

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,8 +1,26 @@
 package com.driver;
 
 public class Boat implements WaterVehicle {
-    String getVehicleName;
-    int getVehicleCapacity;
+    private String getVehicleName;
+    private int getVehicleCapacity;
+
+    public String getGetVehicleName() {
+        return getVehicleName;
+    }
+
+    public void setGetVehicleName(String getVehicleName) {
+        this.getVehicleName = getVehicleName;
+    }
+
+    public int getGetVehicleCapacity() {
+        return getVehicleCapacity;
+    }
+
+    public void setGetVehicleCapacity(int getVehicleCapacity) {
+        this.getVehicleCapacity = getVehicleCapacity;
+    }
+
+
 
     public Boat (String name, int capacity) {
         this.getVehicleName = name;
