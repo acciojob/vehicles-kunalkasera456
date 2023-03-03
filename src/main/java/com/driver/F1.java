6bdbf94ec2e70eabe5987bd6afacd2b6a34b4056
Kunal Kasera <kunalkasera456@gmail.com>
2023-03-03 09:23:54 +0530

    sol_4

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -24,7 +24,7 @@ public class F1 extends Car {
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
             stop();
-            changeGear(1);
+            changeGear(0);
 
         }
 
